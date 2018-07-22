539042262eb6b9b618188b0ae6ebc4011c089f74
wawrze <mateusz.wawreszuk@gmail.com>
2018-07-22 21:03:29 +0200

    working front-end

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -5,10 +5,12 @@ import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class DbService {
 
     @Autowired
