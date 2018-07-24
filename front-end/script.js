dc153d45c35bb83f9aeb7b6ae3583f52da625deb
wawrze <mateusz.wawreszuk@gmail.com>
2018-07-24 23:32:18 +0200

    front-end working with heroku

--- front-end/script.js
+++ front-end/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var apiRoot = 'http://localhost:8080/crud/v1/task/';
+    var apiRoot = 'https://wawrze.herokuapp.com/v1/task/';
     var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     var tasksContainer = $('[data-tasks-container]');
 
