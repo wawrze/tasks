628e9c9c788accccad2ebea246e75c9d35d2516d
wawrze <mateusz.wawreszuk@gmail.com>
2018-07-24 22:25:08 +0200

    jar and postgresql fixed

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -5,16 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+//extends SpringBootServletInitializer
+
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication {
 
 	public static void main(String[] args) {
 	    SpringApplication.run(TasksApplication.class, args);
     }
 
-    @Override
+/*    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	    return application.sources(TasksApplication.class);
-    }
+    }*/
 
 }
