2cad9bf9b431ba73fc053bb52bceec4ce8498b53
wawrze <mateusz.wawreszuk@gmail.com>
2018-08-05 20:02:01 +0200

    18.3

---
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class TrelloDto {
+
+    private int board;
+    private int card;
+
+}
