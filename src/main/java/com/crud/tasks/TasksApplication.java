7533354832c70066afd320335f788098d30880e4
wawrze <mateusz.wawreszuk@gmail.com>
2018-09-26 19:49:23 +0200

    update to Heroku

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,13 +8,13 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class TasksApplication extends SpringBootServletInitializer {
 
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 	    SpringApplication.run(TasksApplication.class, args);
-    }*/
+    }
 
-    @Override
+/*    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	    return application.sources(TasksApplication.class);
-    }
+    }*/
 
 }
